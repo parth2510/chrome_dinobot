ce804bebe01f60bce7c1f90e4cfeec615f13ce13
parth2510 <48416738+parth2510@users.noreply.github.com>
2020-09-13 15:24:16 +0530

    Update and rename pyautogui_algo to pyautogui_algo.py

--- pyautogui_algo.py
+++ pyautogui_algo.py
@@ -4,25 +4,28 @@ import numpy as np
 from PIL import ImageGrab , ImageOps
 
 
-def jump():
+def jump(): ## presses spacebar(for jump) in the keyboard
      pg.keyUp('down') 
      pg.press('space') 
      time.sleep(0.01)
      #pg.keyDown('down')
-    #print('nig')
-def pixelsum():
+    
+def pixelsum(): # presses down to save from flying dinos
     im1=ImageGrab.grab((677,182,1336-450,298))
     grayim1=ImageOps.grayscale(im1)
     ar=np.array(grayim1.getcolors())
     return ar.sum()
-    #print('nigger2')
+    
     
 time.sleep(5)
-while True:
-    if pixelsum() > 26971+1700:
+
+while True: #-->>>> warning- once the game gets over, quickly move the cursor to any corner to stop the script from running <<<--##
+
+    if pixelsum() > 26971+1700: # the sum 
         jump()
-        print('jump',pixelsum(),end= " ")
+        print('jumped',pixelsum(),end= " ")
         time.sleep(0.05)
+        
     elif pixelsum() < 26681+1000:
         pg.keyDown('down')
         #time.sleep(0.01)
