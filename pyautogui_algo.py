572e7eb408a02cb2b6c79573e48634080668c01e
parth2510 <48416738+parth2510@users.noreply.github.com>
2020-09-13 15:35:24 +0530

    Update pyautogui_algo.py

--- pyautogui_algo.py
+++ pyautogui_algo.py
@@ -1,17 +1,18 @@
-#import pyautogui as pg
+import pyautogui as pg
 import time 
 import numpy as np
 from PIL import ImageGrab , ImageOps
 
+#these values of pixel sum are in accrodance with my theme of google chrome.
 
 def jump(): ## presses spacebar(for jump) in the keyboard
      pg.keyUp('down') 
      pg.press('space') 
-     time.sleep(0.01)
+     time.sleep(0.01) ##sleeps for sometime so as to get the tree pass
      #pg.keyDown('down')
     
 def pixelsum(): # presses down to save from flying dinos
-    im1=ImageGrab.grab((677,182,1336-450,298))
+    im1=ImageGrab.grab((677,182,1336-450,298)) #my coordinates
     grayim1=ImageOps.grayscale(im1)
     ar=np.array(grayim1.getcolors())
     return ar.sum()
@@ -19,9 +20,12 @@ def pixelsum(): # presses down to save from flying dinos
     
 time.sleep(5)
 
+# now the main function which captures the screen in intervals of 0.05s and decides what to do
+# and presses the keyboard buttons automatically 
+
 while True: #-->>>> warning- once the game gets over, quickly move the cursor to any corner to stop the script from running <<<--##
 
-    if pixelsum() > 26971+1700: # the sum 
+    if pixelsum() > 26971+1700: # the sum (for me)
         jump()
         print('jumped',pixelsum(),end= " ")
         time.sleep(0.05)
