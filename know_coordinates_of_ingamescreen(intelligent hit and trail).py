29e11bcadb4a87c2bb3a476dbed1295369c64c0e
parth2510 <48416738+parth2510@users.noreply.github.com>
2020-09-13 15:29:10 +0530

    Update know_coordinates_of_ingamescreen(intelligent hit and trail).py

--- know_coordinates_of_ingamescreen(intelligent hit and trail).py
+++ know_coordinates_of_ingamescreen(intelligent hit and trail).py
@@ -1,9 +1,22 @@
+import pyautogui as pg
+import time 
+import numpy as np
+from PIL import ImageGrab , ImageOps
+
+
 i=0 # runs for 5 sec 
-x1= 677 #coordinates of the ingame screen 
+
+# you've to know the coordinates of the dino game in your screen
+# the top left corner of the screen is origin with horizontal axis x( left to right) and vertical axis y (top to down)
+
+
+
+x1= 677 #coordinates of the ingame screen (for me)
 y1=182
 x2=886 //1336-450
 y2=298
 
+# hit and try to get the approximate coordinates, can view the image from line 25 aswell
 while True:
     im1=ImageGrab.grab((x1,y1,x2,y2)) 
     grayim1=ImageOps.grayscale(im1) # convert image to grayscale
